732bbd364d5f001d64024ae74ebcbf658a07f724
JohanMF <JohanMF@dhcp-10-22-189-110.wlan.ntnu.no>
2018-02-01 11:20:39 +0100

    Laget en enkel test...

---
+++ java/app.core/src/test/java/tdt4140/gr18nn/app/core/TestExample.java
@@ -0,0 +1,21 @@
+package tdt4140.gr18nn.app.core;
+
+import junit.framework.TestCase;
+
+public class TestExample extends TestCase {
+	private int a;
+	private int b;
+	public void setup() {
+		a = 5;
+		b = 5;
+	}
+	
+	public void test() {
+		TestCase.assertEquals(a, b);
+	}
+	
+	public void tearDown() {
+		System.out.println("jepp");
+	}
+
+}
