916ef6e276c3f7a8007b2d26125e52a6b99b5d21
nicholvj <3789764+skykanin@users.noreply.github.com>
2018-02-11 14:05:23 +0100

    test change

--- web/src/renderer/components/Welcome/Welcome.test.js
+++ web/src/renderer/components/Welcome/Welcome.test.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Welcome from './Welcome'
 
-test('Welcome component should render as expected', () => {
+/* test('Welcome component should render as expected', () => {
     const wrapper = shallow(<Welcome compiler="Javascript" framework="React" />);
-    //expect(wrapper.contains('Hello from Javascript and React!')).toBe(true);
-})
+    expect(wrapper.contains('Hello from Javascript and React!')).toBe(true);
+}) */
 
-/* describe('<Welcome />', () => {
+describe('<Welcome />', () => {
     it('renders 1 <Welcome /> component', () => {
         const component = shallow(<Welcome compiler="Javascript" framework="React" />);
         expect(component).toHaveLength(1);
     })
-}) */
+})
