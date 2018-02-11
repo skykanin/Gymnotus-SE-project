98033a12d7ba174745a8ecc4f8f0ba6529c589d3
Nicholas Jaunsen <nicholvj@stud.ntnu.no>
2018-02-11 18:50:43 +0100

    changed entire web folder

--- web/src/renderer/components/Welcome/Welcome.test.js
+++ web/src/renderer/components/Welcome/Welcome.test.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Welcome from './Welcome'
 
-//this part doesn't work
-/* test('Welcome component should render as expected', () => {
+test('Welcome component should render as expected', () => {
     const wrapper = shallow(<Welcome compiler="Javascript" framework="React" />);
     expect(wrapper.contains('Hello from Javascript and React!')).toBe(true);
-}) */
+})
 
-describe('<Welcome />', () => {
+/* describe('<Welcome />', () => {
     it('renders 1 <Welcome /> component', () => {
         const component = shallow(<Welcome compiler="Javascript" framework="React" />);
         expect(component).toHaveLength(1);
     })
-})
+}) */
