9862b3f2aec043e640a2d9d2ad4c3defb5eaa563
Nicholas Jaunsen <nicholvj@stud.ntnu.no>
2018-02-23 18:49:43 +0100

    added loginscreen controller and changed FxApp class a bit

---
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/LoginScreenController.java
@@ -0,0 +1,20 @@
+package tdt4140.gr1832.app.ui;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginScreenController {
+    @FXML
+    private JFXTextField username;
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private void handleLoginButtonAction(ActionEvent event) {
+        String uname = username.getText();
+        String pword = password.getText();
+    }
+
+}
