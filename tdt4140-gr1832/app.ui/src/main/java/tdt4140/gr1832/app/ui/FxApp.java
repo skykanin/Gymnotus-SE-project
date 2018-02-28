7934d44362ff85a9e619ba18a1019bc065af7c40
HenrikGiskeFosse <30314898+HenrikGiskeFosse@users.noreply.github.com>
2018-02-28 19:26:34 +0100

    Kobling mellom alle views og opprettelsen av kontrollere til de respektive viewsene, har gått litt ut over branchens intensjon.

--- tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/FxApp.java
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/FxApp.java
@@ -1,37 +1,31 @@
 package tdt4140.gr1832.app.ui;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 
 public class FxApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("TrainerSettings.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("LoginScreen");
+        stage.setTitle("Gymnotus");
         stage.show();
     }
-    
-	@FXML
-	private void TilDashboard(ActionEvent event) throws IOException {
-		Parent LoginScreen_parent = FXMLLoader.load(getClass().getResource("TrainerDashboard.fxml"));
-		Scene LoginScreen_scene = new Scene(LoginScreen_parent);
-		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		app_stage.setScene(LoginScreen_scene);
-		app_stage.show();
-	}
 
-	
     public static void main(String[] args) {
         launch(args);
     }
