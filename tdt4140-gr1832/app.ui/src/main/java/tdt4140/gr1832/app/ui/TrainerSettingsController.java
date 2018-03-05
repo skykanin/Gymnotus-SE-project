a58cc618add5986ba535ff66b3c2a3c7689a0ffc
Sander Grønli Nordeide <sandergn@stud.ntnu.no>
2018-03-05 14:27:04 +0100

    Connected TrainerSettings fxml-Controller-Application

--- tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/TrainerSettingsController.java
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/TrainerSettingsController.java
@@ -2,6 +2,9 @@ package tdt4140.gr1832.app.ui;
 
 import java.io.IOException;
 
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,9 +12,53 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tdt4140.gr1832.app.core.TrainerSettingsApp;
 
-public class TrainerSettingsController {
+public class TrainerSettingsController extends Application {
+	
+	@FXML
+	JFXTextField nameField;
+	
+	@FXML
+	JFXTextField usernameField;
+	
+	@FXML
+	JFXTextField emailField;
+	
+	@FXML
+	JFXTextField tlfField;
+	
+	@FXML
+	JFXTextField ageField;
+	
+//	@FXML
+//	JFXTextField genderField;
+//	
 
+	
+	@FXML
+	public void initialize() {
+		
+		String name = TrainerSettingsApp.getName();
+		String username = TrainerSettingsApp.getUsername();
+		String email = TrainerSettingsApp.getEmail();
+		String tlf = TrainerSettingsApp.getTlf();
+		String age = TrainerSettingsApp.getAge();
+//		String gender = TrainerMemberInfoApp.getGender();
+
+		nameField.setText(name);
+		usernameField.setText(username);		
+		emailField.setText(email);
+		tlfField.setText(tlf);
+		ageField.setText(age);
+//		genderField.setText(gender);
+		
+	}
+	
+	/*
+	  HENRIKs ARBEID START
+	 */
+	
 	@FXML
 	private void TilInnstillinger(ActionEvent event) throws IOException {
 		;
@@ -38,6 +85,25 @@ public class TrainerSettingsController {
 		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		app_stage.setScene(LoginScreen_scene);
 		app_stage.show();
+	}
+	
+	/*
+	 HENRIKs ARBEID SLUTT
+	 */
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("TrainerSettings.fxml")); //Husk å endre til tilhørende fxml fil sitt navn.
+		Scene scene = new Scene(root, 1200, 660);
+        primaryStage.setTitle("MemberInfoView");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(TrainerSettingsController.class, args);
 	}
 	
 }
