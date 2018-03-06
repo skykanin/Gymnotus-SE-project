eb58539a016fd21e6c9b81b1249465001a4f0686
HenrikGiskeFosse <30314898+HenrikGiskeFosse@users.noreply.github.com>
2018-03-06 21:52:28 +0100

    endring i windowcontroller

--- tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/WindowController.java
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/WindowController.java
@@ -11,7 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class WindowController extends Application {
+public class WindowController extends FxApp {
 
 	protected void NavigerTilSide(String filnavn, ActionEvent event) throws IOException {
 		Parent LoginScreen_parent = FXMLLoader.load(getClass().getResource(filnavn));
