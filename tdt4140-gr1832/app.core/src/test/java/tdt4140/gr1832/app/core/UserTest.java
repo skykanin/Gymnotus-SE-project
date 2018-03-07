7609b5f6f3852e227071c2ae2fc0ad5c820abd28
stianismar <stianismar@dhcp-10-24-194-250.wlan.ntnu.no>
2018-03-07 15:32:22 +0100

    #6 Test for User.java written.

--- tdt4140-gr1832/app.core/src/test/java/tdt4140/gr1832/app/core/UserTest.java
+++ tdt4140-gr1832/app.core/src/test/java/tdt4140/gr1832/app/core/UserTest.java
@@ -1,6 +1,7 @@
 package tdt4140.gr1832.app.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +12,7 @@ public class UserTest {
 	
 	@Before
 	public void setUp() throws Exception{
-		this.user = new User("OlaN", "fortniteHhamstring","Ola Nordmann", 12, 0,  "ola.nordmann@gmail.com", "41546593");
+		this.user = new User("OlaN", "hamstring","Ola Nordmann", 12, 0,  "ola.nordmann@gmail.com", "41546593");
 		
 	}
 	
@@ -20,7 +21,17 @@ public class UserTest {
 		assertEquals("41546593", user.getPhone());
 			 
 		}
-	
+	@Test
+	public void testSetPhone() {
+	user.setPhone("41546593");
+	assertTrue("41546593" == user.getPhone());
+		 
+	}
+	@Test
+	public void testGetGender() {
+	assertEquals(0, user.getGender());
+	}
+		 
 	@Test
 	public void testSetGender() {
 		try {
@@ -31,13 +42,97 @@ public class UserTest {
 			assert true;
 		}		
 	}
+	@Test
+	public void testSetEmail() {
+		try {
+			user.setEmail("sdt.tds@gmail.c@om");
+			System.out.println("Should have thrown IllegalARgumentException.");
+			assert false;
+		}catch (IllegalArgumentException e) {
+			assert true;
+	}
+	}
+	@Test
+	public void testGetEmail() {
+	assertEquals("ola.nordmann@gmail.com", user.getEmail());
+	}
+	
+	@Test
+	public void testSetName() {
+		try {
+			user.setName("gdsgs7gdsg");
+			System.out.println("Should have thrown IllegalARgumentException.");
+			assert false;
+		}catch (IllegalArgumentException e) {
+			assert true;
+		}
+	}
+		
+		@Test
+		public void testGetName() {
+		assertEquals("Ola Nordmann", user.getName());
+		}
+		
+		@Test
+		public void testSetUsername() {
+			try {
+				user.setUsername("gdsgsd gsdg");
+				System.out.println("Should have thrown IllegalARgumentException.");
+				assert false;
+			}catch (IllegalArgumentException e) {
+				assert true;
+			}
+		}
+		
+		@Test
+		public void testGetUsername() {
+		assertEquals("OlaN", user.getUsername());
+		}
+		
+		
+		@Test
+		public void testSetAge() {
+			try {
+				user.setAge(124);
+				System.out.println("Should have thrown IllegalARgumentException.");
+				assert false;
+			}catch (IllegalArgumentException e) {
+				assert true;
+			}
+		}
+		@Test
+		public void testGetAge() {
+		assertEquals(12, user.getAge());
+		}
+		
+		@Test
+		public void testSetPassword() {
+			try {
+				user.setPassword("gs");
+				System.out.println("Should have thrown IllegalARgumentException.");
+				assert false;
+			}catch (IllegalArgumentException e) {
+				assert true;
+			}
+		}
+		@Test
+		public void testGetPassword() {
+		assertEquals("hamstring", user.getPassword());
+		}
+		
+}
+	
+	
+	
+	
+	
 //	@Test
 //	public void testSetMaxPuse() {
 //athlete.setMaxPulse(230);
 //assertTrue(230 == athlete.getMaxPulse);
 		
 	//}
-}
+
 	
 		
 		
