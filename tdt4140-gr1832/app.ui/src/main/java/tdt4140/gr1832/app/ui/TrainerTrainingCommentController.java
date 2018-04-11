4b665cb54e8519d10f305e9acd286c002d608ac2
JohanMF <JohanMF@dhcp-10-24-4-77.wlan.ntnu.no>
2018-04-11 16:38:29 +0200

    Added UI structure for issue 35

---
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/TrainerTrainingCommentController.java
@@ -0,0 +1,69 @@
+package tdt4140.gr1832.app.ui;
+
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class TrainerTrainingCommentController extends WindowController {
+
+    @FXML
+    private StackPane root;
+
+    @FXML
+    public void initialize() {
+    		root.setPickOnBounds(false);
+    }
+    
+    @FXML
+    public void loadDialog(ActionEvent parentEvent) {
+        JFXDialogLayout content = new JFXDialogLayout();
+        content.setHeading(new Text("Logg ut bekreftelse"));
+        content.setBody(new Text("Er du sikker på at du vil logge ut?"));
+        JFXDialog dialog = new JFXDialog(root, content, JFXDialog.DialogTransition.CENTER);
+        JFXButton buttonYes = new JFXButton("Ja");
+        JFXButton buttonNo = new JFXButton("Nei");
+
+        buttonYes.setOnAction((event) -> {
+            dialog.close();
+            try {
+                NavigerTilSide("LoginScreen.fxml", parentEvent);
+                FxApp.getAS().DUMMYsetuser(null);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+
+        buttonNo.setOnAction((event) -> {
+            dialog.close();
+        });
+        content.setActions(buttonYes, buttonNo);
+        dialog.show();
+    }
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TrainerTrainingComment.fxml"));
+        
+        stage.setTitle("FXML Welcome");
+        stage.setScene(new Scene(root, 1200, 660));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
