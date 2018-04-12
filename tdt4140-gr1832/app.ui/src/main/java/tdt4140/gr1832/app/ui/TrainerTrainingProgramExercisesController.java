01750f6a531a128fd686ede3865088a211c41b3e
Sander Grønli Nordeide <sandergn@stud.ntnu.no>
2018-04-13 00:48:57 +0200

    "added ExercisesController"

--- tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/TrainerTrainingProgramExercisesController.java
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/TrainerTrainingProgramExercisesController.java
@@ -9,14 +9,10 @@ import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
+
 
 public class TrainerTrainingProgramExercisesController extends WindowController implements Initializable {
 	
