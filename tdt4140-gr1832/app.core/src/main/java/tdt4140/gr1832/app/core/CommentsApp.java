a98ef88664a1bf3d316878f715376129c1402771
stianismar <stianismar@dhcp-10-22-17-54.wlan.ntnu.no>
2018-04-13 15:25:17 +0200

    Branch is up and running!

---
+++ tdt4140-gr1832/app.core/src/main/java/tdt4140/gr1832/app/core/CommentsApp.java
@@ -0,0 +1,71 @@
+package tdt4140.gr1832.app.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class CommentsApp {
+	private List<ShowCommentsContainer> containerComments = new ArrayList<ShowCommentsContainer>();
+	private String baseURI = "http://146.185.153.244:8080/api/";
+	
+	public void requestProgramComments(int id) {
+		
+		Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(baseURI+"comment/get_comments_from_program?program_id="+id);
+        String test = webTarget.request(MediaType.APPLICATION_JSON).get(String.class);
+        Gson gson = new Gson();
+        containerComments = gson.fromJson(test, new TypeToken<List<ShowCommentsContainer>>(){}.getType());
+
+
+	}
+	public List<String> getCommentList() {
+		TrainerMemberInfoApp a = new TrainerMemberInfoApp();
+		
+		List<String> l = new ArrayList<>();
+		for (ShowCommentsContainer containerComment : containerComments ) {
+			a.requestUserInformation_ID(Integer.toString(containerComment.getUserID()));
+			String n= a.getName();
+			l.add(n +", " + containerComment.getDate() + ": " + containerComment.getContent());
+		}
+		return l;
+	}
+	
+	public ShowCommentsContainer getProgramCommentsContainer(int i) {
+		return containerComments.get(i);
+	}
+	
+	public int getContainerExcerciseProgramLength() {
+		return (int)containerComments.size();
+	}
+
+	//public void addContainerTocontainerExercisePrograms(ShowCommentsContainer container) {
+	//	containerComments.add(container);
+		
+	//}
+
+	public Object getBaseURI() {
+		return baseURI;
+	}
+	
+	
+	
+	public String toString() {
+		return ""+containerComments;
+	}
+public static void main ( String[] args) {
+	//System.out.println("WHAT");
+
+	CommentsApp a = new CommentsApp();
+	a.requestProgramComments(2);
+	System.out.println(a.getCommentList());
+}
+
+
+}
