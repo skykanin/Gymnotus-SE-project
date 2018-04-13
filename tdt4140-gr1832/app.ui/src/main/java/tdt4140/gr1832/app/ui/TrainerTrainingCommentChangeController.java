ad879108a7174794dbc34824508e1eb0913965fb
JohanMF <JohanMF@10.0.0.104>
2018-04-13 11:13:12 +0200

    Bug fix UI tests

---
+++ tdt4140-gr1832/app.ui/src/main/java/tdt4140/gr1832/app/ui/TrainerTrainingCommentChangeController.java
@@ -0,0 +1,17 @@
+package tdt4140.gr1832.app.ui;
+
+import javafx.fxml.FXML;
+
+public class TrainerTrainingCommentChangeController {
+
+	
+	@FXML
+	public void handleComment() {
+		
+	}
+	
+	@FXML
+	public void handleChangeComment() {
+		
+	}
+}
